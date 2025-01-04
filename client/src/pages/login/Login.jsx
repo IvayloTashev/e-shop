d73c806d6aa6b5d1f9553af788faaecc540fcb82
IvayloTashev <i.tashev@abv.bg>
2025-01-04 14:49:46 +0200

    Add navbar and footer to login and register page

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,22 +1,29 @@
 import React from 'react'
 import './Login.css'
+import Navbar from '../../components/navbar/Navbar'
+import Footer from '../../components/footer/Footer'
 
 const Login = () => {
     return (
-        <div className='login-container'>
-            <div className='login-wrapper'>
-                <h1>Sign up</h1>
-                <form action="">
-                    <input type="text" placeholder='email' />
-                    <input type="password" placeholder='password' />
-                    <button>Login</button>
-                </form>
-                <div className='login-links'>
-                    <a href="">Create an new account.</a>
-                    <a href="">Forget password?</a>
+        <div>
+            <Navbar />
+            <div className='login-container'>
+                <div className='login-wrapper'>
+                    <h1>Sign up</h1>
+                    <form action="">
+                        <input type="text" placeholder='email' />
+                        <input type="password" placeholder='password' />
+                        <button>Login</button>
+                    </form>
+                    <div className='login-links'>
+                        <a href="">Create an new account.</a>
+                        <a href="">Forget password?</a>
+                    </div>
                 </div>
             </div>
+            <Footer />
         </div>
+
     )
 }
 
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
 import './Register.css'
+import Navbar from '../../components/navbar/Navbar'
+import Footer from '../../components/footer/Footer'
 
 const Register = () => {
-  return (
-    <div className='register-container'>
-        <div className='register-wrapper'>
-            <h1>Create an account</h1>
-            <form action="">
-                <input type="text" placeholder='username' />
-                <input type="text" placeholder='email' />
-                <input type="password" placeholder='password' />
-                <input type="password" placeholder='repeat password' />
-                <p>By creating an account, I consent to the processing of my personal data in accordance with the <b>PRIVACY POLICY</b></p>
-                <button>Register</button>
-            </form>
+    return (
+        <div>
+            <Navbar />
+            <div className='register-container'>
+                <div className='register-wrapper'>
+                    <h1>Create an account</h1>
+                    <form action="">
+                        <input type="text" placeholder='username' />
+                        <input type="text" placeholder='email' />
+                        <input type="password" placeholder='password' />
+                        <input type="password" placeholder='repeat password' />
+                        <p>By creating an account, I consent to the processing of my personal data in accordance with the <b>PRIVACY POLICY</b></p>
+                        <button>Register</button>
+                    </form>
+                </div>
+            </div>
+            <Footer />
         </div>
-    </div>
-  )
+    )
 }
 
 export default Register
