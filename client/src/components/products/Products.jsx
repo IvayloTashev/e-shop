ad3e81db6df8159c9cba4ffc413917bcedb1e85a
IvayloTashev <i.tashev@abv.bg>
2025-01-05 14:07:06 +0200

    Implement fetching data, filtering and sorting functionality

--- client/src/components/products/Products.jsx
+++ client/src/components/products/Products.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
 import './Products.css'
-
-import { popularProducts } from '../../dummyData.js'
+import React, { useEffect, useState } from 'react'
+import { getAllProducts } from '../../hooks/useProducts.js'
 import ProductsItem from './products-item/ProductsItem.jsx'
 
-const Products = () => {
+const Products = ({ category, filters, sort }) => {
+    const [products] = getAllProducts(category);
+    const [filteredProducts, setFilteredProducts] = useState([]);
+
+    useEffect(() => {
+        if (category) {
+            setFilteredProducts(
+                products.filter((item) =>
+                    Object.entries(filters).every(([key, value]) =>
+                        item[key].includes(value)))
+            )
+        }
+    }, [products, category, filters]);
+
+    useEffect(() => {
+        const sortFunctions = {
+            newest: (a, b) => a.createdAt - b.createdAt,
+            lowToHigh: (a, b) => a.price - b.price,
+            highToLow: (a, b) => b.price - a.price,
+        };
+    
+        setFilteredProducts((prev) => [...prev].sort(sortFunctions[sort]));
+    }, [sort]);
+
     return (
         <div className='products-container'>
-            {popularProducts.map((item) => (
-                <ProductsItem item={item} key={item.id}/>
-            ))}
+            {category
+                ? filteredProducts.map((item) => <ProductsItem item={item} key={item._id} />)
+                : products.slice(0, 8).map((item) => <ProductsItem item={item} key={item._id} />)
+            }
         </div>
     )
 }
