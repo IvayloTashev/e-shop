2bf346fc4d76c823f92bdb3c2610d39de11d3912
IvayloTashev <i.tashev@abv.bg>
2025-01-06 19:12:03 +0200

    Implement get one product in details page

--- client/src/pages/details/Details.jsx
+++ client/src/pages/details/Details.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Details.css'
 
 import Navbar from '../../components/navbar/Navbar'
 import Announcement from '../../components/announcement/Announcement'
 import Newsletter from '../../components/newsletter/Newsletter'
 import Footer from '../../components/footer/Footer'
+import { useLocation } from 'react-router-dom'
+import { getOneProduct } from '../../hooks/useProducts'
 
 const Details = () => {
+    const location = useLocation();
+    const id = location.pathname.split('/')[2];
+    const [product] = getOneProduct(id);
+    const [quantity, setQuantity] = useState(1);
+    const [color, setColor] = useState(null);
+
+    const handleQuality = (type) => {
+        if (type === "decrease") {
+            if (quantity > 1) {
+                setQuantity(quantity - 1);
+            }
+
+        } else {
+            setQuantity(quantity + 1);
+        }
+    };
+
     return (
         <div className='details-container'>
             <Announcement />
@@ -14,25 +33,29 @@ const Details = () => {
 
             <div className='details-wrapper'>
                 <div className='details-image-holder'>
-                    <img src="https://desktop.bg/system/images/437733/normal/920012159.jpg" alt="" />
+                    <img src={product.img} alt="image" />
                 </div>
 
                 <div className='details-info-holder'>
-                    <h1>PRO X TKL LIGHTSPEED</h1>
-                    <p>A championship-trusted wireless gaming keyboard designed for the highest levels of competitive play. Designed with pros and engineered to win.</p>
-                    <p>$134.99</p>
+                    <h1>{product.title}</h1>
+                    <p>{product.description}</p>
+                    <p>${product.price}</p>
                     <div className='details-color-selector'>
                         <p>Select Color:</p>
-                        <div className='color-selector'></div>
-                        <div className='color-selector'></div>
-                        <div className='color-selector'></div>
+                        {product.color?.map((c) => (
+                            <div className='color-selector'
+                                key={c}
+                                style={{ backgroundColor: c }}
+                                onClick={() => setColor(c)}
+                            ></div>
+                        ))}
                     </div>
 
                     <div className='details-cart-container'>
                         <div className='amount-container'>
-                            <p className='remove'>-</p>
-                            <p className='amount'>1</p>
-                            <p className='add'>+</p>
+                            <button className='remove' onClick={() => handleQuality('decrease')}>-</button>
+                            <p className='amount'>{quantity}</p>
+                            <button className='add' onClick={() => handleQuality('increase ')}>+</button>
                         </div>
 
                         <button>ADD TO CART</button>
