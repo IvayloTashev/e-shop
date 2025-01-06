0fefbc032f0ec8a038f48bc179f032c52918c06b
IvayloTashev <i.tashev@abv.bg>
2025-01-06 19:10:55 +0200

    Add get one product hook

--- client/src/hooks/useProducts.js
+++ client/src/hooks/useProducts.js
@@ -1,17 +1,19 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-
-let url = 'http://localhost:5000/api/products';
+import { publicRequest, userRequest } from '../constants/requestMethods'
 
 export function getAllProducts(category) {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
         (async () => {
+            let url = '';
 
             if (category) {
                 url = `http://localhost:5000/api/products?category=${category}`
-            };
+            } else {
+                url = `http://localhost:5000/api/products`
+            }
 
             try {
                 const { data } = await axios.get(url);
@@ -25,4 +27,24 @@ export function getAllProducts(category) {
     }, [category]);
 
     return [products];
+}
+
+export function getOneProduct(id) {
+    const [product, setProduct] = useState({});
+
+    useEffect(() => {
+        (async () => {
+
+            try {
+                const { data } = await publicRequest.get(`products/find/${id}`);
+                setProduct(data);
+
+            } catch (err) {
+                console.error('Error fetching product:', err);
+            }
+
+        })();
+    }, [id]);
+
+    return [product];
 }
