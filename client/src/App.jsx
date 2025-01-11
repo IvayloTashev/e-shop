8a7a979af61913b0163b9b84c00b291040593403
IvayloTashev <i.tashev@abv.bg>
2025-01-11 20:07:55 +0200

    Implement login functionality in the component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,28 @@
-import { Route, Routes } from "react-router-dom";
 import './App.css'
+import { Navigate, Route, Routes } from "react-router-dom";
 import Home from './pages/home/Home';
 import Catalog from './pages/catalog/Catalog';
 import Details from './pages/details/Details';
 import Register from './pages/register/Register';
 import Login from './pages/login/Login';
 import Cart from './pages/cart/Cart';
+import { useSelector } from "react-redux";
 
 const App = () => {
+    const user = useSelector((state) => state.user.currentUser);
+
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
 
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+            <Route path="/register" element={user ? <Navigate to="/" /> : <Register />} />
 
             <Route path="/products/:categories" element={<Catalog />} />
             <Route path="/product/:id" element={<Details />} />
             <Route path="/cart" element={<Cart />} />
-            
+
         </Routes>
 
     )
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
 import './Login.css'
+import React, { useState } from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import Footer from '../../components/footer/Footer'
+import { login } from '../../redux/apiCalls'
+import { useDispatch, useSelector } from 'react-redux'
+
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const dispatch = useDispatch();
+    const { isFetching, error } = useSelector((state) => state.user)
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        login(dispatch, { email, password });
+
+    }
+
     return (
         <div>
             <Navbar />
             <div className='login-container'>
                 <div className='login-wrapper'>
                     <h1>Sign up</h1>
+                    {error && (<p className='errorMsg'>Something went wrong...</p>)}
                     <form action="">
-                        <input type="text" placeholder='email' />
-                        <input type="password" placeholder='password' />
-                        <button>Login</button>
+                        <input type="text" placeholder='email' onChange={(e) => setEmail(e.target.value)} />
+                        <input type="password" placeholder='password' onChange={(e) => setPassword(e.target.value)} />
+                        <button
+                            onClick={handleLogin}
+                            disabled={isFetching}
+                        >Login</button>
                     </form>
                     <div className='login-links'>
                         <a href="">Create an new account.</a>
