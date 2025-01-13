e53bdaaabd5d9f1021bd283bebf741eae0743714
IvayloTashev <i.tashev@abv.bg>
2025-01-13 21:05:03 +0200

    Add chart component

---
+++ admin/src/components/chart/Chart.jsx
@@ -0,0 +1,26 @@
+import './Chart.css'
+import React from 'react'
+import { LineChart, Line, XAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
+
+const Chart = ({ title, data, dataKey, grid }) => {
+
+    return (
+        <div className='chart-container'>
+            <div className='chart'>
+                <h2 className='chart-title'>{title}</h2>
+                <ResponsiveContainer width="100%" aspect={4 / 1}>
+                    <LineChart data={data}>
+                        <XAxis dataKey={"name"} stroke='#5a5ad1' />
+                        <Line type="monotone" dataKey={dataKey} stroke='#5a5ad1' />
+                        <Tooltip />
+                        {grid && <CartesianGrid stroke='#e0dfdf' strokeDasharray='5 5' />}
+
+                    </LineChart>
+                </ResponsiveContainer>
+            </div>
+        </div>
+    )
+}
+
+export default Chart
