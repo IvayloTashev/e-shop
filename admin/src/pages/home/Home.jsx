58efabc8130debed99f9d91e4252627d226714fe
IvayloTashev <i.tashev@abv.bg>
2025-01-14 20:20:51 +0200

    Implement widget components in Home page

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -5,12 +5,18 @@ import FeatureInfo from '../../components/feature-info/FeatureInfo'
 
 
 import React from 'react'
+import WidgetSmall from '../../components/widget-small/WidgetSmall'
+import WidgetLarge from '../../components/widget-large/WidgetLarge'
 
 const Home = () => {
     return (
         <div className='home-container'>
             <FeatureInfo />
             <Chart data={dummyData} title={"User Analytics"} dataKey={"Active User"} grid />
+            <div className='home-widgets'>
+                <WidgetSmall />
+                <WidgetLarge />
+            </div>
         </div>
     )
 }
