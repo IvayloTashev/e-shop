5c7aa7fa68036937c337878ce231789dbf92ae23
IvayloTashev <i.tashev@abv.bg>
2025-01-15 21:00:47 +0200

    Add react-router-dom

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,20 +1,27 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
 import Home from './pages/home/Home';
+import UserList from './pages/user-list/UserList';
+
 
 function App() {
-  return (
-    <div>
-      <Topbar />
+    return (
+        <div>
+            <Topbar />
 
-      <div className='app-container'>
-        <Sidebar />
-        <Home />
-      </div>
+            <div className='app-container'>
+                <Sidebar />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/users' element={<UserList />} />
+                </Routes>
 
-    </div>
-  );
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
