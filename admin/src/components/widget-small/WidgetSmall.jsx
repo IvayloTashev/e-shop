7582aa5a304a82637eb4340ae1c0e6da1ca9e926
IvayloTashev <i.tashev@abv.bg>
2025-01-21 19:48:08 +0200

    Populate WidgetSmall component with dynamic data

--- admin/src/components/widget-small/WidgetSmall.jsx
+++ admin/src/components/widget-small/WidgetSmall.jsx
@@ -1,71 +1,36 @@
 import './WidgetSmall.css'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import VisibilityIcon from '@mui/icons-material/Visibility';
+import { userRequest } from '../../constants/requestMethods';
 
 const WidgetSmall = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const res = await userRequest.get('users/?new=true');
+            setUsers(res.data);
+        })();
+    }, []);
+
     return (
         <div className='widget-small-container'>
             <h2 className='widget-small-title'>New Join Members</h2>
             <ul className='widget-small-list'>
-                <li className='widget-small-list-item'>
-                    <img className='widget-small-img' src="https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg" alt="avatar" />
-                    <div className='widget-small-user-info'>
-                        <p className='widget-small-name'>Brut Cosmos</p>
-                        <p className='widget-small-job-title'>Fullstack Developer</p>
-                    </div>
-                    <button className='widget-small-btn'>
-                        <VisibilityIcon />
-                        Display
-                    </button>
-                </li>
+                {users.map((user) => (
+                    <li className='widget-small-list-item' key={user._id}>
+                        <img className='widget-small-img' src={user.image || "https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1114445501.jpg"} alt="avatar" />
+                        <div className='widget-small-user-info'>
+                            <p className='widget-small-name'>{user.username}</p>
+                            <p className='widget-small-job-title'>{user.email}</p>
+                        </div>
+                        <button className='widget-small-btn'>
+                            <VisibilityIcon />
+                            Display
+                        </button>
+                    </li>
+                ))};
 
-                <li className='widget-small-list-item'>
-                    <img className='widget-small-img' src="https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg" alt="avatar" />
-                    <div className='widget-small-user-info'>
-                        <p className='widget-small-name'>Brut Cosmos</p>
-                        <p className='widget-small-job-title'>Fullstack Developer</p>
-                    </div>
-                    <button className='widget-small-btn'>
-                        <VisibilityIcon />
-                        Display
-                    </button>
-                </li>
-
-                <li className='widget-small-list-item'>
-                    <img className='widget-small-img' src="https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg" alt="avatar" />
-                    <div className='widget-small-user-info'>
-                        <p className='widget-small-name'>Brut Cosmos</p>
-                        <p className='widget-small-job-title'>Fullstack Developer</p>
-                    </div>
-                    <button className='widget-small-btn'>
-                        <VisibilityIcon />
-                        Display
-                    </button>
-                </li>
-
-                <li className='widget-small-list-item'>
-                    <img className='widget-small-img' src="https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg" alt="avatar" />
-                    <div className='widget-small-user-info'>
-                        <p className='widget-small-name'>Brut Cosmos</p>
-                        <p className='widget-small-job-title'>Fullstack Developer</p>
-                    </div>
-                    <button className='widget-small-btn'>
-                        <VisibilityIcon />
-                        Display
-                    </button>
-                </li>
-
-                <li className='widget-small-list-item'>
-                    <img className='widget-small-img' src="https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg" alt="avatar" />
-                    <div className='widget-small-user-info'>
-                        <p className='widget-small-name'>Brut Cosmos</p>
-                        <p className='widget-small-job-title'>Fullstack Developer</p>
-                    </div>
-                    <button className='widget-small-btn'>
-                        <VisibilityIcon />
-                        Display
-                    </button>
-                </li>
             </ul>
 
         </div>
