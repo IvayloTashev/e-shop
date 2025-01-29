7fdf53e6379629db6c425fbff4b058403938bdff
IvayloTashev <i.tashev@abv.bg>
2025-01-29 19:29:59 +0200

    Add update product function

--- admin/src/pages/product/Product.jsx
+++ admin/src/pages/product/Product.jsx
@@ -3,14 +3,17 @@ import React, { useEffect, useMemo, useState } from 'react'
 import Chart from '../../components/chart/Chart'
 import { productDummyData } from '../../dummyData'
 import { Link, useLocation } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { userRequest } from '../../constants/requestMethods'
+import { updaeteProduct } from '../../redux/apiCalls'
 
 
 const Product = () => {
     const location = useLocation();
+    const dispatch = useDispatch();
     const productId = location.pathname.split('/')[2];
     const [productStats, setProductStats] = useState([]);
+    const [inputs, setInputs] = useState({});
 
     const product = useSelector((state) => state.product.products.find((product) => product._id === productId));
     //TODO FIX IT LATER
@@ -22,7 +25,6 @@ const Product = () => {
         (async () => {
             try {
                 const res = await userRequest.get(`orders/income?pid=${productId}`);
-                console.log(res);
 
                 const list = res.data.sort((a, b) => {
                     return a._id - b._id
@@ -37,6 +39,18 @@ const Product = () => {
             }
         })();
     }, [productId, months])
+
+    const handleChange = (e) => {
+        setInputs((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    const handleUpdate = (e) => {
+        e.preventDefault();
+        updaeteProduct(dispatch, inputs, productId)
+    }
 
     return (
         <div className='product-container'>
@@ -80,16 +94,16 @@ const Product = () => {
                 <form className='product-bot-form'>
                     <div className='product-form-left'>
                         <label className='product-form-title'>Product Name</label>
-                        <input className='product-form-input' type="text" placeholder={product.title} />
+                        <input name='title' className='product-form-input' type="text" placeholder={product.title} onChange={handleChange} />
 
                         <label className='product-form-title'>Product Description</label>
-                        <input className='product-form-input' type="text" placeholder={product.description} />
+                        <input name='description' className='product-form-input' type="text" placeholder={product.description} onChange={handleChange} />
 
                         <label className='product-form-title'>Product Price</label>
-                        <input className='product-form-input' type="text" placeholder={product.price} />
+                        <input name='price' className='product-form-input' type="text" placeholder={product.price} onChange={handleChange} />
 
                         <label className='product-form-title'>In Stock</label>
-                        <select className='product-form-input' name="instock" id="idStock">
+                        <select className='product-form-input' name="instock" id="idStock" onChange={handleChange}>
                             <option value="yes">Yes</option>
                             <option value="no">No</option>
                         </select>
@@ -101,7 +115,7 @@ const Product = () => {
                             <label for="file"></label>
                             <input type="file" id='file' />
                         </div>
-                        <button className='product-upload-btn'>Update</button>
+                        <button className='product-upload-btn' onClick={handleUpdate}>Update</button>
                     </div>
                 </form>
             </div>
